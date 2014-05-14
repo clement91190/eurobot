bcf702d4d1e5167e5b1e609de0d9f8cee1c53a3d
clement91190 <clem.jambou@gmail.com>
2014-05-14 17:01:43 +0000

    small changes

--- pi/server/server.py
+++ pi/server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, send_file
+import time
 from vision import vision
 from socket import socket, AF_INET, SOCK_DGRAM
 
@@ -12,9 +13,14 @@ mySocket = socket(AF_INET, SOCK_DGRAM)
 
 @app.route("/get_triangles_center")
 def get_triangles_center():
+    take_shot()
+    time.sleep(0.5)
     angle = request.args.get('angle')
+    couleur = request.args.get('couleur')
     if angle is None:
         angle = 30
+    if couleur is None:
+	couleur = 'R'
     angle = int(angle)
     return "{}".format(vision.process_img(angle))
 
@@ -22,11 +28,14 @@ def get_triangles_center():
 @app.route("/take_shot")
 def take_shot():
     mySocket.sendto('shot', (SERVER_IP, PORT_NUMBER))
+    return ""
 
 
 @app.route('/get_image')
 def get_image():
-    return send_file('vision/shot.jpg', mimetype='image/jpg')
+    take_shot()
+    time.sleep(0.5)
+    return send_file('vision/shotx.jpg', mimetype='image/jpg')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
